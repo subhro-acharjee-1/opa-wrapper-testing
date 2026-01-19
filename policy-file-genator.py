7d75990cbfada84588aa98ea83c128bf9a8794c2
Subhro Acharjee <subhro.acharjee@hashicorp.com>
2026-01-19 14:47:46 +0530

    1200 policies added

--- policy-file-genator.py
+++ policy-file-genator.py
@@ -13,7 +13,7 @@ policies = [
     ("public_exposure_risk", "data.terraform.public_exposure_risk.rule", "mandatory"),
 ]
 
-for i in range(1, 200):
+for i in range(1, 100):
     for name, query, level in policies:
         print(f'''policy {name}_{i} {{
 query = "{query}"
